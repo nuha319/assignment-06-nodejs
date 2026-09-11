fa339fdc00936aa27a52ebadf8f7943d905bc144
nuha319 <nuhaamerica2001@gmail.com>
2026-09-12 02:51:28 +0600

    Add Blog, About  & Contact Route

--- index.js
+++ index.js
@@ -25,6 +25,72 @@ const server = http.createServer((req, res) => {
 </body>
 </html>
     `);
+  } else if (req.url == "/blog") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Page</title>
+</head>
+<body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/blog">Blog</a>
+        <a href="/contact">Contact</a>
+        <a href="/about">About</a>
+    </nav>
+
+    <h1>Blog Page</h1>
+
+    <p> Welcome to the Blog Page. This is a simple website created using Node.js and the HTTP module.</p>
+</body>
+</html>`);
+  } else if (req.url == "/contact") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Page</title>
+</head>
+<body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/blog">Blog</a>
+        <a href="/contact">Contact</a>
+        <a href="/about">About</a>
+    </nav>
+
+    <h1>Contact Page</h1>
+
+    <p> Welcome to the Contact Page. This is a simple website created using Node.js and the HTTP module.</p>
+</body>
+</html>`);
+  } else if (req.url == "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Page</title>
+</head>
+<body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/blog">Blog</a>
+        <a href="/contact">Contact</a>
+        <a href="/about">About</a>
+    </nav>
+
+    <h1>About Page</h1>
+
+    <p> Welcome to the About Page. This is a simple website created using Node.js and the HTTP module.</p>
+</body>
+</html>`);
   }
 });
 
