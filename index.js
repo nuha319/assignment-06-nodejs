1db35f4a15e099c57987741840e4d80aaaf87a91
nuha319 <nuhaamerica2001@gmail.com>
2026-09-12 02:38:17 +0600

    Add Home Route

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 const http = require("http");
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  // Homepage
+  if (req.url == "/") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(`
+            <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+</head>
+<body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/blog">Blog</a>
+        <a href="/contact">Contact</a>
+        <a href="/about">About</a>
+    </nav>
+
+    <h1>Home Page</h1>
+
+    <p> Welcome to the Home Page. This is a simple website created using Node.js and the HTTP module.</p>
+</body>
+</html>
+    `);
+  }
+});
+
 server.listen(5000, () => {
-  console.log("Server is running on port 5000");
+  console.log("Server is running on 5000");
 });
